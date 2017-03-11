a1fded879e9375bb51fa146fbe1426293ecbe611
Stefan Seifried <s.seifried@gmx.at>
2017-03-11 15:29:31 +0100

    removed all traces from old greensock api

--- js/index.js
+++ js/index.js
@@ -7,11 +7,6 @@ PathAnimation.setlog0(".reset_active");
 PathAnimation.setlog0(".set_active");
 PathAnimation.setlog0(".q_active");
 
-// test code
-//PathAnimation.log0(".q_not_active");
-//PathAnimation.log1(".q_active");
-//PathAnimation.tpd(".nor_bottom_spinner");
-
 // logical wirinfg
 var reset = new LogicWire(".reset_active",false);
 var q_not = new LogicWire(".q_not_active",true);
@@ -21,38 +16,13 @@ var set = new LogicWire(".set_active", false);
 var nor1 = new LogicNOR(".nor_top_spinner", reset, q_not, q);
 var nor2 = new LogicNOR(".nor_bottom_spinner", q, set, q_not);
 
-// function to set a line log1
-window.com.greensock.core.Animation.prototype.log1 = function (target) {
-  return this.to(target, ANIMATION_TIME, {drawSVG: "100%", ease: Power0.easeNone});
-};
-
-// function to set a line log0
-window.com.greensock.core.Animation.prototype.log0 = function (target) {
-  return this.to(target, ANIMATION_TIME, {drawSVG: "100% 100%", ease: Power0.easeNone})
-  .set(target, {drawSVG: "-2%"})
-}
-
-// function to visualize a propagation dely through a logic gate
-window.com.greensock.core.Animation.prototype.tpd = function (target) {
-  return this.set(target, {visibility: "visible"})
-    .to(target, ANIMATION_TIME, {rotation:"+=360", transformOrigin:"45% 50%"})
-    .set(target, {visibility: "hidden"});
-}
-
+// control functions
 function setfn() {
   if($("#set").is(':checked')) {
     set.setState(true);
   } else {
     set.setState(false);
   }
-  // create animation timeline
-  /*var animation = new TimelineLite();
-  animation.log1(".set_active")
-  .tpd(".nor_bottom_spinner")
-  .log0(".q_not_active")
-  .tpd(".nor_top_spinner")
-  .log1(".q_active")
-  .log0(".set_active");*/
 }
 
 function resetfn() {
@@ -61,15 +31,6 @@ function resetfn() {
   } else {
     reset.setState(false);
   }
-
-  // create animation timeline
-  /*var animation = new TimelineLite();
-  animation.log1(".reset_active")
-  .tpd(".nor_top_spinner")
-  .log0(".q_active")
-  .tpd(".nor_bottom_spinner")
-  .log1(".q_not_active")
-  .log0(".reset_active");*/
 }
 
 function metafn() {
@@ -80,26 +41,6 @@ function metafn() {
     set.setState(false);
     reset.setState(false);
   }
-  // create animation timeline
-  /*var animation = new TimelineLite();
-  animation.log1(".reset_active,.set_active")
-  .tpd(".nor_top_spinner,.nor_bottom_spinner")
-  .log0(".reset_active,.set_active")
-  .log1(".q_active,.q_not_active")
-  .tpd(".nor_top_spinner,.nor_bottom_spinner")
-  .log0(".q_active,.q_not_active")
-  .tpd(".nor_top_spinner,.nor_bottom_spinner")
-  .log1(".q_active,.q_not_active")
-  .tpd(".nor_top_spinner,.nor_bottom_spinner")
-  .log0(".q_active,.q_not_active")
-  .tpd(".nor_top_spinner,.nor_bottom_spinner")
-  .log1(".q_active,.q_not_active")
-  .tpd(".nor_top_spinner,.nor_bottom_spinner")
-  .log0(".q_active,.q_not_active")
-  .tpd(".nor_top_spinner,.nor_bottom_spinner")
-  .log1(".q_active,.q_not_active")
-  .tpd(".nor_top_spinner,.nor_bottom_spinner")
-  .log0(".q_active,.q_not_active");*/
 }
 
 $("#set").change(setfn);
